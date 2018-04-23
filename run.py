7fcd128828e8c066fc7641b066d2eaf049e12833
alaya <lvwenlong_lambda@qq.com>
2018-04-23 16:41:55 +0800

    Use sigmoid as default

--- run.py
+++ run.py
@@ -10,13 +10,13 @@ test_x = np.loadtxt('test_x').T
 test_y = np.loadtxt('test_y')
 test_y = test_y.reshape(1, test_y.size)
 
-layer_sizes = [60, 200]
-activations = [dsk.relu, dsk.relu]
+layer_sizes = [60]
+activations = [dsk.sigmoid]
 scale       = 0.1
 
 dim = train_x.shape[0]
 
-gp       = dsk.DSK_GP(train_x, train_y, layer_sizes, activations, bfgs_iter=200, l1=0, debug=True);
+gp       = dsk.DSK_GP(train_x, train_y, layer_sizes, activations, bfgs_iter=100, l1=0, debug=True);
 theta    = scale * np.random.randn(gp.num_param)
 theta[0] = np.log(np.std(train_y) / 2)
 theta[1] = np.log(np.std(train_y) / 2)
